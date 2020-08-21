54d7c6727a8c56ae5424cde2648463056c2e5d80
travis-mcminimy <tmcminimy@gmail.com>
2020-08-21 10:42:18 -0700

    Boy am I awful at maps and forEach...on plus side, classes was a breeze

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -20,8 +20,10 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
+
 const displayNames = [];
 console.log(displayNames);
+
 
 /* Request 2: .map()
 
@@ -29,7 +31,12 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = function(animoos){
+  return animoos.map(function(animals){
+    const lowerCaseAnimals{};
+    lowerCaseAnimals['Animal Names'] = animals.animal_name + " " + animals.scientific_name;
+  })
+}
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,16 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// Because both are inside the function 'myFunction' and it does not need to reach 'outside' to do so.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(number){
+  var sum = 0;
+  for(var i = 1; i <= number; i++){
+    sum += i;
+  }
+  return sum;
+}
+console.log(summation(4));
